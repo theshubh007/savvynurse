4cd8ca0af24c26c4d3daebebb6749c5485aff8bd
shubham <thekingshubh07@gmail.com>
2023-07-28 20:58:16 +0530

    pub

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
